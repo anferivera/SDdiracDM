4c0fc34e271cdbeff0f4ab5e81b57679e1128405
anferivera <anferivera@gmail.com>
2019-02-20 16:53:14 -0500

    SI and coannhilation analysis

--- random_general_scan3.py
+++ random_general_scan3.py
@@ -46,7 +46,7 @@ for n in range(0,Num):
     LamSP = np.exp(np.random.uniform(np.log(10**(-3)),np.log(10**(1))))  
     vS = np.exp(np.random.uniform(np.log(10**(2)),np.log(10**(5))))
     YRD = np.exp(np.random.uniform(np.log(10**(-5)),np.log(10**(0))))*signo()
-    YRC = np.exp(np.random.uniform(np.log(10**(-3)),np.log(10**(1))))*signo()
+    YRC = np.exp(np.random.uniform(np.log(3.*10**(-3)),np.log(10**(1))))*signo()
     MDF = np.exp(np.random.uniform(np.log(10**(2)),np.log(10**(4))))
     
     ###  NEUTRINO EXPERIMENTAL VALUES ###################################################
