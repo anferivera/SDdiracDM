35675bfe494b1d9a2b9a87bf99f3d6b73d1bdd3c
anferivera <anferivera@gmail.com>
2019-04-21 22:08:17 -0500

    21-4 home changes

---
+++ pareto.py
@@ -0,0 +1,17 @@
+def pareto_frontier(Xs, Ys, maxX = True, maxY = True):
+# Sort the list in either ascending or descending order of X
+    myList = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxX)
+# Start the Pareto frontier with the first value in the sorted list
+    p_front = [myList[0]]    
+# Loop through the sorted list
+    for pair in myList[1:]:
+        if maxY: 
+            if pair[1] >= p_front[-1][1]: # Look for higher values of Y
+                p_front.append(pair) # and add them to the Pareto frontier
+        else:
+            if pair[1] <= p_front[-1][1]: # Look for lower values of Y
+                p_front.append(pair) # and add them to the Pareto frontier
+# Turn resulting pairs back into a list of Xs and Ys
+    p_frontX = [pair[0] for pair in p_front]
+    p_frontY = [pair[1] for pair in p_front]
+    return p_frontX, p_frontY
