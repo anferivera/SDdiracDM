c627167735722bb330d5af1654f4d793386f42fe
anferivera <anferivera@gmail.com>
2019-06-03 18:04:07 -0500

    3-6 home

---
+++ random_general_scan5.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+
+import pyslha
+import pySDdiracDM_LesHouches_generator
+import numpy as np
+import pandas as pd
+import subprocess
+import time
+import sys
+
+import NEUTRINO2018
+import neutrino_analytic3
+
+#Generate positive and negative numbers
+def signo():
+    #Generar  \pm1
+    if np.random.uniform(-1.,1.) < 0:
+        signo = -1
+    else :
+        signo = 1
+    return signo
+
+# Tiempo inicial
+t1=time.time() 
+
+#Open xdict which is now a <PySLHA Doc: 10 blocks, 0 decays, 0 xsections>
+xdict = pySDdiracDM_LesHouches_generator.buildSLHAinFile()
+
+x=[]
+
+#Num is the times to run the program. Entry using the keyboard
+Num = int(sys.argv[1])
+
+for n in range(0,Num):
+
+    #Random parameters as input  
+    #BLOCK MINPAR
+    vevSM = 244.874425
+    MS12 = np.exp(np.random.uniform(np.log(10**(6)),np.log(4.*10**(8))))
+    MS22 = MS12
+    LamS1H = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(-1))))
+    LamS2H = LamS1H
+    LamSPH = np.exp(np.random.uniform(np.log(10**(-4)),np.log(10**(-2))))
+    #mh2 = LamSP*vS -> LamS no cero
+    LamS = np.exp(np.random.uniform(np.log(10**(-4)),np.log(3.)))    
+    LamSP = np.exp(np.random.uniform(np.log(10**(-3)),np.log(3.)))  
+    vS = np.exp(np.random.uniform(np.log(10**(2)),np.log(10**(5))))
+    YRD = np.exp(np.random.uniform(np.log(10**(-6)),np.log(3.)))*signo()
+    YRC = np.exp(np.random.uniform(np.log(3.*10**(-3)),np.log(3.)))*signo()
+    MDF = np.exp(np.random.uniform(np.log(10**(2)),np.log(1.*10**(4))))
+    
+    # Strong cut: when DM is almost a pure doublet -> U11~1 (no run SPHENO)
+    if vS*YRC/np.sqrt(2) > 1500.:
+        continue    
+    
+    ###  NEUTRINO EXPERIMENTAL VALUES ###################################################
+    #phases of the PMNS matrix and the R 
+    phases1 = np.random.uniform(0.,0.0*np.pi,3) # WARNING! They are in zero
+    delta = phases1[0]
+    eta1 = phases1[1]
+    eta2 = phases1[2]
+    #light neutrino masses (up 3 sigma range) NH (NO)
+    #mnu1 = 10**((np.log10(2.5e-3)-np.log10(1e-9))*np.random.uniform(0,1)+np.log10(1e-9))*1e-9 
+    #Zero in this framework
+    mnu1 = 1.0e-20
+    mnu2 = np.sqrt(np.random.uniform(7.05e-5,8.14e-5)*1.0e-18+mnu1**2)
+    mnu3 = np.sqrt(np.random.uniform(2.41e-3,2.60e-3)*1.0e-18+mnu1**2)
+    #mixing angles (up 3 sigma range) NH
+    t12 = np.arcsin(np.sqrt(np.random.uniform(0.273,0.379)))
+    t23 = np.arcsin(np.sqrt(np.random.uniform(0.445,0.599)))
+    t13 = np.arcsin(np.sqrt(np.random.uniform(0.0196,0.0241)))
+    #Building PMNS matrix NH
+    UM12 = np.array([ [np.cos(t12),np.sin(t12),0.], [-np.sin(t12),np.cos(t12),0.], [0.,0.,1.0] ])
+    UM13 = np.array([ [np.cos(t13),0.,np.sin(t13)], [0.,1.0,0.], [-np.sin(t13),0.,np.cos(t13)] ])
+    UM23 = np.array([ [1.0,0.,0.], [0.,np.cos(t23),np.sin(t23)], [0.,-np.sin(t23),np.cos(t23)] ])
+    Uphases = np.array([ [np.exp(eta1*1j),0.,0.], [0.,np.exp(eta2*1j),0.], [0.,0.,1.0] ])
+    U=np.dot(UM23,np.dot(UM13,np.dot(UM12,Uphases)))
+    #Defining the U elementes. readeable
+    U11 = np.real(U[0,0])
+    U12 = np.real(U[0,1])
+    U13 = np.real(U[0,2])
+    U21 = np.real(U[1,0])
+    U22 = np.real(U[1,1])
+    U23 = np.real(U[1,2])
+    U31 = np.real(U[2,0])
+    U32 = np.real(U[2,1])
+    U33 = np.real(U[2,2])
+    
+    ### Algoritm to compute LAMBDAi elements
+    MS1 = np.sqrt(LamS1H*vevSM**2/2.+MS12)
+    MS2 = np.sqrt(LamS2H*vevSM**2/2.+MS22)
+    M1 = neutrino_analytic3.MATRIXCHIDIAG(MDF,vS,YRC,YRD)['M1']
+    M2 = neutrino_analytic3.MATRIXCHIDIAG(MDF,vS,YRC,YRD)['M2']
+    vv12 = neutrino_analytic3.MATRIXCHIDIAG(MDF,vS,YRC,YRD)['V12']
+    vv22 = neutrino_analytic3.MATRIXCHIDIAG(MDF,vS,YRC,YRD)['V22']
+    uu11 = neutrino_analytic3.MATRIXCHIDIAG(MDF,vS,YRC,YRD)['U11']
+    uu21 = neutrino_analytic3.MATRIXCHIDIAG(MDF,vS,YRC,YRD)['U21']
+    L1 = neutrino_analytic3.LAMBDA(M1,MS1,vv12,uu11)+ neutrino_analytic3.LAMBDA(M2,MS1,vv22,uu21)
+    L2 = neutrino_analytic3.LAMBDA(M1,MS2,vv12,uu11)+ neutrino_analytic3.LAMBDA(M2,MS2,vv22,uu21)
+    
+    #####  ALGORITM: Particular case(see): inversion-neutrino-matrix.nb ######################
+    a11 = 0.0
+    a12 = 0.0 
+    ### Random -> FREE PARAMETERS
+    a21 = np.exp(np.random.uniform(np.log(10**(-6)),np.log(10**(0))))*signo()
+    a22 = np.exp(np.random.uniform(np.log(10**(-6)),np.log(10**(0))))*signo()
+    a31 = np.exp(np.random.uniform(np.log(10**(-6)),np.log(10**(0))))*signo()    
+    a32 = np.exp(np.random.uniform(np.log(10**(-6)),np.log(10**(0))))*signo()
+    ### Solved in Mathematica
+    b11 = -((a32*mnu2*U12 - a22*mnu3*U13)/(a22*a31*L1 - a21*a32*L1))   
+    b21 = -((a32*mnu2*U22 - a22*mnu3*U23)/(a22*a31*L1 - a21*a32*L1))
+    b31 = -((a32*mnu2*U32 - a22*mnu3*U33)/(a22*a31*L1 - a21*a32*L1))
+    b12 = (a31*mnu2*U12 - a21*mnu3*U13)/(a22*a31*L2 - a21*a32*L2)
+    b22 = (a31*mnu2*U22 - a21*mnu3*U23)/(a22*a31*L2 - a21*a32*L2)
+    b32 = (a31*mnu2*U32 - a21*mnu3*U33)/(a22*a31*L2 - a21*a32*L2)
+
+    # Cut, perturbativity in bij
+    if np.abs(b11) > 3. or np.abs(b21) > 3.:
+        continue
+    if np.abs(b31) > 3. or np.abs(b12) > 3.:
+        continue
+    if np.abs(b22) > 3. or np.abs(b32) > 3.:
+        continue 
+    
+    ###  Modify the LesHouches file   
+    xdict.blocks['MINPAR'].entries[2]='%.7E    # MDFInput' %MDF
+    xdict.blocks['MINPAR'].entries[3]='%.7E    # MS12Input' %MS12
+    xdict.blocks['MINPAR'].entries[4]='%.7E    # MS22Input' %MS22
+    xdict.blocks['MINPAR'].entries[5]='%.7E    # LamS1HInput' %LamS1H
+    xdict.blocks['MINPAR'].entries[6]='%.7E    # LamS2HInput' %LamS2H
+    xdict.blocks['MINPAR'].entries[7]='%.7E    # LamSPHInput' %LamSPH
+    xdict.blocks['MINPAR'].entries[8]='%.7E    # LamSInput' %LamS
+    xdict.blocks['MINPAR'].entries[9]='%.7E    # LamSPInput' %LamSP
+    xdict.blocks['MINPAR'].entries[10]='%.7E    # vSInput' %vS
+    xdict.blocks['MINPAR'].entries[11]='%.7E    # YRDInput' %YRD
+    xdict.blocks['MINPAR'].entries[12]='%.7E    # YRCInput' %YRC
+    
+    ###  Modify the LesHouches file 
+    xdict.blocks['YRA1IN'].entries[1]='%.6E    # YRA1(1)' %a11  
+    xdict.blocks['YRA1IN'].entries[2]='%.6E    # YRA1(2)' %a21    
+    xdict.blocks['YRA1IN'].entries[3]='%.6E    # YRA1(3)' %a31     
+    xdict.blocks['YRA2IN'].entries[1]='%.6E    # YRA2(1)' %a12  
+    xdict.blocks['YRA2IN'].entries[2]='%.6E    # YRA2(2)' %a22     
+    xdict.blocks['YRA2IN'].entries[3]='%.6E    # YRA2(3)' %a32        
+    xdict.blocks['YRB1IN'].entries[1]='%.6E    # YRB1(1)' %b11  
+    xdict.blocks['YRB1IN'].entries[2]='%.6E    # YRB1(2)' %b21     
+    xdict.blocks['YRB1IN'].entries[3]='%.6E    # YRB1(3)' %b31 
+    xdict.blocks['YRB2IN'].entries[1]='%.6E    # YRB2(1)' %b12  
+    xdict.blocks['YRB2IN'].entries[2]='%.6E    # YRB2(2)' %b22     
+    xdict.blocks['YRB2IN'].entries[3]='%.6E    # YRB2(3)' %b32      
+    
+    ###  Write the Leshouches file
+    pyslha.writeSLHAFile('LesHouches.in.SDdiracDM_low',xdict)
+    
+    ### Run SPheno
+    spheno = subprocess.getoutput('../.././SPheno-4.0.3/bin/SPhenoSDdiracDM LesHouches.in.SDdiracDM_low')
+    SPheno_output = subprocess.getoutput('cat SPheno.spc.SDdiracDM')
+    
+    mCha = eval(SPheno_output.split('# Fe_3')[1].split()[21])
+    mChi1 = eval(SPheno_output.split('# Fe_3')[1].split()[25])
+    mChi2 = eval(SPheno_output.split('# Fe_3')[1].split()[29])
+    mh1 = eval(SPheno_output.split('Block MASS')[1].split()[9])
+    mh2 = eval(SPheno_output.split('Block MASS')[1].split()[13])  
+       
+    # Cut in mChi1 < 3 TeV -> Bias
+    if mChi1 > 1500.:
+        continue
+      
+    # cut in m_higgses bigger than 124.5 (heavy higgses)
+    if mh1 < 124.5 or mh2 < 124.5:
+        continue
+    
+    ##++++++++++ Neutrinos masses +++++++++++++++++++++++++++++++++
+    if SPheno_output.split('# Fe_3')[1].split()[1] == "NaN":
+        continue
+    else: 
+        mv1 = eval(SPheno_output.split('# Fe_3')[1].split()[1])            
+    mv2 = eval(SPheno_output.split('# Fe_3')[1].split()[5])       
+    mv3 = eval(SPheno_output.split('# Fe_3')[1].split()[9])  
+    
+    ###  Run micromegas and take the relic density. 
+    mo = subprocess.getoutput('~/Work/micromegas_4.2.5/SDdiracDM/./CalcOmega') #Only DM
+    
+    if len(mo.split()) == 2:
+        continue
+    
+    Omega = eval(mo.split('Omega h^2=')[1].split()[0])
+    #print("n=",n,"Omega=",Omega)
+    
+    # Choose Omega to 5 sigma 2018
+    if Omega > 0.1250:
+        continue
+    if Omega < 0.11499999:
+        continue
+
+    mo = subprocess.getoutput('~/Work/micromegas_4.2.5/SDdiracDM/./CalcOmega_with_DI_Detection') #Full
+    
+    ### Direct Detection: CDM-nucleon cross sections[pb]
+    PSI = eval(mo.split('CDM-nucleon cross sections[pb]:')[1].split()[2])
+    PSD = eval(mo.split('CDM-nucleon cross sections[pb]:')[1].split()[4])
+    NSI = eval(mo.split('CDM-nucleon cross sections[pb]:')[1].split()[7])
+    NSD = eval(mo.split('CDM-nucleon cross sections[pb]:')[1].split()[9])
+    ### Indirect Detection: annihilation cross section vcs[cm^3/s]
+    sv = eval(mo.split('annihilation cross section')[1].split()[0])  
+    
+    mS1 = eval(SPheno_output.split('# Fe_3')[1].split()[13])
+    mS2 = eval(SPheno_output.split('# Fe_3')[1].split()[17])
+    
+    XV11 = eval(SPheno_output.split('Block XV')[1].split()[6])   
+    XV12 = eval(SPheno_output.split('Block XV')[1].split()[11])
+    XV21 = eval(SPheno_output.split('Block XV')[1].split()[16])
+    XV22 = eval(SPheno_output.split('Block XV')[1].split()[21])
+    XU11 = eval(SPheno_output.split('Block XU')[1].split()[6])
+    XU12 = eval(SPheno_output.split('Block XU')[1].split()[11])
+    XU21 = eval(SPheno_output.split('Block XU')[1].split()[16])
+    XU22 = eval(SPheno_output.split('Block XU')[1].split()[21])
+    vevSM = eval(SPheno_output.split('Block HMIX')[1].split()[6])
+    
+    ### EWPO
+    T = eval(SPheno_output.split('Block SPhenoLowEnergy')[1].split()[5])
+    S = eval(SPheno_output.split('Block SPhenoLowEnergy')[1].split()[11])
+    U = eval(SPheno_output.split('Block SPhenoLowEnergy')[1].split()[17])
+    ### LFV
+    mueg = eval(SPheno_output.split('Block FlavorKitLFV')[1].split()[6])
+    taumug = eval(SPheno_output.split('Block FlavorKitLFV')[1].split()[16])
+    #Scalar Higgs mixing
+    ZH11 = eval(SPheno_output.split('Block SCALARMIX ')[1].split()[6])
+    ZH12 = eval(SPheno_output.split('Block SCALARMIX ')[1].split()[11])
+    ZH21 = eval(SPheno_output.split('Block SCALARMIX ')[1].split()[16])
+    ZH22 = eval(SPheno_output.split('Block SCALARMIX ')[1].split()[21])
+    
+    #Higgs to two photons
+    if len(SPheno_output.split('BR(hh_1 -> VP VP )'))==1:
+        BRh1gg = 0.
+    else:
+        r = len(SPheno_output.split('BR(hh_1 -> VP VP )')[0].split())
+        BRh1gg = eval(SPheno_output.split('BR(hh_1 -> VP VP )')[0].split()[r-5])
+
+    if len(SPheno_output.split('BR(hh_2 -> VP VP )'))==1:
+        BRh2gg = 0.
+    else:
+        r = len(SPheno_output.split('BR(hh_2 -> VP VP )')[0].split())
+        BRh2gg = eval(SPheno_output.split('BR(hh_2 -> VP VP )')[0].split()[r-5])        
+
+    x.append([MDF,MS12,MS22,LamS1H,LamS2H,LamSPH,LamS,LamSP,vS,YRD,YRC,a11,a21,a31,a12,a22,a32,\
+              b11,b21,b31,b12,b22,b32,mh1,mh2,mv3,mv2,mv1,mS1,mS2,mCha,mChi1,mChi2,\
+              XV11,XV12,XV21,XV22,XU11,XU12,XU21,XU22,vevSM,Omega,PSI,PSD,NSI,NSD,T,S,U,mueg,taumug,sv,\
+              ZH11,ZH12,ZH21,ZH22,BRh1gg,BRh2gg])
+    
+x=np.asarray(x)
+xd=pd.DataFrame(x,columns=['MDF','MS12','MS22','LamS1H','LamS2H','LamSPH','LamS','LamSP','vS','YRD'\
+                           ,'YRC','YRA11','YRA12','YRA13','YRA21','YRA22','YRA23','YRB11','YRB12','YRB13'\
+                           ,'YRB21','YRB22','YRB23','mh1','mh2','mv3','mv2','mv1','mS1','mS2','mCha'\
+                           ,'mChi1','mChi2','XV11','XV12','XV21','XV22','XU11','XU12','XU21','XU22','vevSM'\
+                          ,'Omega','PSI','PSD','NSI','NSD','T','S','U','mueg','taumug','sv','ZH11','ZH12','ZH21'\
+                           ,'ZH22','BRh1gg','BRh2gg'])
+
+#argv[2] will be the number in the end of the file extension: example: xd_scan1.csv
+xd.to_csv('xd_scan-general-'+sys.argv[2]+'.csv')
+
+t4=time.time() 
+print ("The program spent", t4-t1, "s running",Num,"times -> xd_scan",sys.argv[2],".csv")
